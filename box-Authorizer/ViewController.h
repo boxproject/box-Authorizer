4e09ce246c0fa87ea3a8e21d3ea556f8a42bc061
rony.huang@2se.com <rony.huang@2se.com>
2018-02-26 10:56:47 +0800

    Initial Commit

---
+++ box-Authorizer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  box-Authorizer
+//
+//  Created by Rony on 2018/2/26.
+//  Copyright © 2018年 2se. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ box-Authorizer/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  box-Authorizer
+//
+//  Created by Rony on 2018/2/26.
+//  Copyright © 2018年 2se. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
